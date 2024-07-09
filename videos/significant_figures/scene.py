e846228a3e694bbcaf8699e351a4a917672aa844
João Vitor Witt <joaovitorwitt2004@gmail.com>
2024-07-08 22:28:46 -0300

    first commit

--- videos/significant_figures/scene.py
+++ videos/significant_figures/scene.py
@@ -1,8 +1,33 @@
-from manim import *
+from manim import * # pylint: disable=unused-wildcard-import, wildcard-import
 
+class OpeningQuote(Scene):
 
-class CreateCircle(Scene):
     def construct(self):
-        circle = Circle()  # create a circle
-        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-        self.play(Create(circle))  # show the circle on screen
+        quote = Text(
+            "''Science cannot solve the ultimate mystery of nature because \nwe ourselves are a part of the mystery that we are trying to solve.''",
+            font_size=30,
+            
+        )
+
+        quote.to_edge(UP)
+
+        for mob in quote.submobjects:
+            mob.set_color(WHITE)
+
+        author = Text(
+            "— Max Planck"
+        )
+
+        author.set_color(YELLOW)
+        author.next_to(quote, DOWN, buff=0.5)
+
+        self.play(Write(quote, run_time=2))
+        self.wait(1)
+        self.play(Write(author, run_time=4))
+
+        self.wait()
+
+
+class HistoricalContext(Scene):
+    def construct(self):
+        pass
